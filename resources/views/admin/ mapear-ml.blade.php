ff52389877a831285a79ffb2b17ba1ff8deb9202
Derek <dereka.espinosa@gmail.com>
2026-02-07 21:07:49 -0600

    fix: Agregar vista de mapear ML

--- resources/views/admin/ mapear-ml.blade.php
+++ resources/views/admin/ mapear-ml.blade.php
@@ -9,7 +9,7 @@
                 <svg class="h-4 w-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
                 </svg>
-                Volver al Dashboard
+                Volver al Dashboardd
             </a>
         </div>
     </x-slot>
